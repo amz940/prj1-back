3ad813a556316eb9f9f1efb25071e1d383f22cc6
amz940 <dh_bug@naver.com>
2023-11-09 11:45:53 +0900

    최초 설정 및 BoardController post /api/board/add 작성

---
+++ src/main/java/com/example/prj1back/Prj1BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.prj1back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj1BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prj1BackApplication.class, args);
+    }
+
+}
