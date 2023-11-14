e00d6adfbe10673c40cd6289ae63bcadee025cb1
amz940 <dh_bug@naver.com>
2023-11-14 14:07:40 +0900

    게시글 볼때 member의 nickName이 writer에 표시 (버그 수정)

--- src/main/java/com/example/prj1back/mapper/BoardMapper.java
+++ src/main/java/com/example/prj1back/mapper/BoardMapper.java
@@ -16,7 +16,8 @@ public interface BoardMapper {
     @Select("""
             SELECT b.id,
             b.title,
-            m.nickName `writer`,
+            b.writer,
+            m.nickName,
             b.inserted
             FROM board b JOIN member m ON b.writer = m.id
             ORDER BY b.id DESC
@@ -27,7 +28,8 @@ public interface BoardMapper {
             SELECT b.id,
             b.title,
             b.content,
-            m.nickName `writer`,
+            b.writer,
+            m.nickName,
             b.inserted
             FROM board b JOIN member m ON b.writer = m.id
             WHERE b.id = #{id}
