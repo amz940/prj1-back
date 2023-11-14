1ef67e7f71318972fc7f519c6875bd87edd8ad58
amz940 <dh_bug@naver.com>
2023-11-14 12:27:25 +0900

    게시글 볼 떄 member의 nickName이 writer에 보이게 설정

--- src/main/java/com/example/prj1back/mapper/BoardMapper.java
+++ src/main/java/com/example/prj1back/mapper/BoardMapper.java
@@ -14,16 +14,23 @@ public interface BoardMapper {
     int insert(Board board);
 
     @Select("""
-            SELECT id, title, writer, inserted
-            FROM board
-            ORDER BY id DESC
+            SELECT b.id,
+            b.title,
+            m.nickName `writer`,
+            b.inserted
+            FROM board b JOIN member m ON b.writer = m.id
+            ORDER BY b.id DESC
             """)
     List<Board> selectAll();
 
     @Select("""
-            SELECT id, title, content, writer, inserted
-            FROM board
-            WHERE id = #{id}
+            SELECT b.id,
+            b.title,
+            b.content,
+            m.nickName `writer`,
+            b.inserted
+            FROM board b JOIN member m ON b.writer = m.id
+            WHERE b.id = #{id}
             """)
     Board selectById(Integer id);
 
@@ -33,18 +40,18 @@ public interface BoardMapper {
             """)
     int deleteById(Integer id);
 
-@Update("""
-        UPDATE board
-        SET title =#{title},
-            content = #{content},
-            writer = #{writer}
-            WHERE id = #{id}
-        """)
+    @Update("""
+            UPDATE board
+            SET title =#{title},
+                content = #{content},
+                writer = #{writer}
+                WHERE id = #{id}
+            """)
     int update(Board board);
 
-@Delete("""
-        DELETE FROM board
-        WHERE writer = #{writer}
-        """)
+    @Delete("""
+            DELETE FROM board
+            WHERE writer = #{writer}
+            """)
     int deleteByWriter(String writer);
 }
