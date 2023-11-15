0dfcfee4563a3f6020f4e14ec0194bd4cbc200d9
amz940 <dh_bug@naver.com>
2023-11-15 10:00:05 +0900

    댓글 쓰기 기능
    CREATE TABLE comment (
        id INT PRIMARY KEY AUTO_INCREMENT,
        boardId INT NOT NULL REFERENCES board(id),
        memberId VARCHAR(100) NOT NULL REFERENCES member(id),
        comment VARCHAR(500) NOT NULL ,
        inserted DATETIME NOT NULL DEFAULT NOW()
    );

---
+++ src/main/java/com/example/prj1back/controller/CommentController.java
@@ -0,0 +1,35 @@
+package com.example.prj1back.controller;
+
+import com.example.prj1back.domain.Comment;
+import com.example.prj1back.domain.Member;
+import com.example.prj1back.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/comment")
+public class CommentController {
+
+    private final CommentService service;
+
+    @PostMapping("add")
+    public ResponseEntity add(@RequestBody Comment comment,
+                                      @SessionAttribute(value = "login", required = false) Member login){
+
+        if (login == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        if ( service.validate(comment)) {
+            if (service.add(comment, login)) {
+                return ResponseEntity.ok().build();
+            } else {
+                return ResponseEntity.internalServerError().build();
+            }
+        }
+
+        return null;
+    }
+}
