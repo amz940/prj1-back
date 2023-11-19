6cb10e1a1acca387d71ac927e79a6eabf736f113
amz940 <dh_bug@naver.com>
2023-11-19 21:24:47 +0900

    유틸리티 클래스 작성, Board.getAgo 메소드 내용을 Util로 옮긴 후 comment와 board에 작성

--- src/main/java/com/example/prj1back/domain/Board.java
+++ src/main/java/com/example/prj1back/domain/Board.java
@@ -1,5 +1,6 @@
 package com.example.prj1back.domain;
 
+import com.example.prj1back.utill.AppUtil;
 import lombok.Data;
 
 import java.time.Duration;
@@ -19,31 +20,7 @@ public class Board {
     private LocalDateTime inserted;
 
     public String getAgo(){
-        // 새로운 프로퍼티를 만들어서 사용가능
-        LocalDateTime now = LocalDateTime.now();
-
-        if (inserted.isBefore(now.minusYears(1))){
-            Period between = Period.between(inserted.toLocalDate(), now.toLocalDate());
-            return between.get(ChronoUnit.YEARS) + "년 전";
-        } else if (inserted.isBefore(now.minusMonths(1))) {
-            Period between = Period.between(inserted.toLocalDate(), now.toLocalDate());
-            return between.get(ChronoUnit.MONTHS) + "달 전";
-        } else if (inserted.isBefore(now.minusDays(1))) {
-            Period between = Period.between(inserted.toLocalDate(), now.toLocalDate());
-            return between.get(ChronoUnit.DAYS) + "일 전";
-        } else if (inserted.isBefore(now.minusHours(1))) {
-            Duration between = Duration.between(inserted.toLocalDate(), now.toLocalDate());
-            return (between.getSeconds() / 60 / 60) + "시간 전";
-        } else if (inserted.isBefore(now.minusMinutes(1))) {
-            Duration between = Duration.between(inserted, now);
-            return (between.getSeconds() / 60) + "분 전";
-        }
-        else {
-            Duration between = Duration.between(inserted,now);
-            return between.getSeconds() + "초 전";
-        }
-
+        return AppUtil.getAgo(inserted, LocalDateTime.now());
     }
-
 
 }
